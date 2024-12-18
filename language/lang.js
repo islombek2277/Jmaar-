04a8fe569ea9375849b41566921301c1ba5220b5
islombek2277 <xujamovislom13@gmail.com>
2024-12-18 18:26:29 +0500

    language selector

--- language/lang.js
+++ language/lang.js
@@ -1,18 +1,18 @@
 const translations = {
-    "en": {
-      "selected": "Portfolio",
-      "nav-item-1":"Residential complex Nurafshon",
-      "nav-item-2":"Customs terminal",
-      "nav-item-3":"FACTORY AHK furniture production",
-      "nav-item-4":"Nature Grow furniture production",
-      "contact":"Contact"
-    },
-    "ru": {
-      "selected": "Портфолио",
-      "nav-item-1":"Жилой комплекс Нурафшон",
-      "nav-item-2":"Таможный терминал",
-      "nav-item-3":"FACTORY AHK производствa мебели",
-      "nav-item-4":"Nature Grow производствa мебели",
-      "contact":"Контакт"
-    }
+  "en": {
+    "selected": "Portfolio",
+    "nav-item-1":"Residential complex Nurafshon",
+    "nav-item-2":"Customs terminal",
+    "nav-item-3":"FACTORY AHK furniture production",
+    "nav-item-4":"Nature Grow furniture production",
+    "contact":"Contact"
+  },
+  "ru": {
+    "selected": "Портфолио",
+    "nav-item-1":"Жилой комплекс Нурафшон",
+    "nav-item-2":"Таможный терминал",
+    "nav-item-3":"FACTORY AHK производствa мебели",
+    "nav-item-4":"Nature Grow производствa мебели",
+    "contact":"Контакт"
+  }
 }
