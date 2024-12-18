3880fc27144bf2d3c249952e4b66ec49b0b7957f
islombek2277 <xujamovislom13@gmail.com>
2024-12-18 19:08:11 +0500

    translate

--- language/lang.js
+++ language/lang.js
@@ -5,7 +5,10 @@ const translations = {
     "nav-item-2":"Customs terminal",
     "nav-item-3":"FACTORY AHK furniture production",
     "nav-item-4":"Nature Grow furniture production",
-    "contact":"Contact"
+    "contact":"Contact",
+    "about-h1":"Commercial construction company",
+    "about-p":"JMAR GREAT PARTNERS - is a young, dynamically developing construction company, specializing especially in the construction of industrial facilities. We create modern, reliable and functional production sites that meet all business requirements. In addition, our company is successfully engaged in the construction of residential multi-storey buildings, bringing quality and innovative solutions to each project. We also provide subcontracting services, which allows our partners to effectively implement the most complex projects.",
+
   },
   "ru": {
     "selected": "Портфолио",
@@ -13,6 +16,8 @@ const translations = {
     "nav-item-2":"Таможный терминал",
     "nav-item-3":"FACTORY AHK производствa мебели",
     "nav-item-4":"Nature Grow производствa мебели",
-    "contact":"Контакт"
+    "contact":"Контакт",
+    "about-h1":"Компания по строительству коммерческих объектов",
+    "about-p":"JMAR GREAT PARTNERS  – молодая, динамично развивающаяся строительная компания, специализирующаяся особенно на возведении промышленных объектов. Мы создаем современные, надежные и функциональные производственные площадки, отвечающие всем требованиям бизнеса. Кроме того, наша компания успешно занимается строительством жилых многоэтажных домов, привнося качество и инновационные решения в каждый проект. Также мы оказываем услуги субподрядных работ, что позволяет нашим партнерам эффективно реализовывать самые сложные проекты.",
   }
 }
