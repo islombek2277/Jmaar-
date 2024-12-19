6f79e59734eb28e6fb6e3bcb267d3366ea7be0e2
islombek2277 <xujamovislom13@gmail.com>
2024-12-19 18:49:06 +0500

    ru en

--- language/lang.js
+++ language/lang.js
@@ -9,7 +9,114 @@ const translations = {
     "about-h1":"Commercial construction company",
     "about-p":"JMAR GREAT PARTNERS - is a young, dynamically developing construction company, specializing especially in the construction of industrial facilities. We create modern, reliable and functional production sites that meet all business requirements. In addition, our company is successfully engaged in the construction of residential multi-storey buildings, bringing quality and innovative solutions to each project. We also provide subcontracting services, which allows our partners to effectively implement the most complex projects.",
 
-  },
+   // -----------result section translate-----------
+    "sq-1": "Concrete poured",
+    "sq-2": "2 380 tons",
+    "sq-3": "Connected reinforcement",
+    "sq-4": "475 tons",
+    "sq-5": "Metal structures assembled",
+    "sq-6": "Built",
+
+    // ----------nurafshon section translate---------
+    "nur-1": "Nurafshon Residential Complex",
+    "nur-2": "Its total area is 4 900 m²",
+    "nur-3": "Start of construction: 2021 ",
+    "nur-4": "Completion of construction: 2024 ",
+    "nur-5": "Under construction:",
+    "nur-6": "Concrete poured: 1 550 m³",
+    "nur-7": "Rebar tied: 140 tons",
+    "nur-8": "Cost: 1.3 million USD",
+   
+
+        // ----------infra section translate---------
+    "inf-1": "Infrastructure",
+    "inf-2": "28 apartments",
+    "inf-3": "1 005 m² of non-residential premises",
+    "inf-4": "Playground",
+    "inf-5": "Parking",
+
+
+    // ----------terminal section translate---------
+    "ter-1": "Logistics terminal",
+    "ter-2": "The area of ​​the facility is 22 hectares",
+    "ter-3": "Construction start: 2023 ",
+    "ter-4": "Planned completion: 2026 ",
+    "ter-5": "Under construction:",
+    "ter-6": "Concrete poured: 26 190 m³ ",
+    "ter-7": "Rebar tied: 1 850 tons",
+    "ter-8": "Under construction will be:",
+    "ter-9": "Concrete poured: 38 000 m³ ",
+    "ter-10": "Rebar tied: 2 148 tons",
+    "ter-11": "Metal structures assembled: 420 tons",
+    "ter-12": "Project cost: 30.2 million USD",
+
+
+
+    // ---------infra section logistic terminal--------------
+    "infra-1": "Infrastructure",
+    "infra-2": "6 000 м² office",
+    "infra-3": "106 000 м² container platform",
+    "infra-4": "2 gantry cranes",
+    "infra-5": "4 570 м railway tracks",
+
+
+
+    //------------factory section translate------------------
+  "fac-1": "FACTORY AHK",
+  "fac-2": "Furniture manufacturing factory",
+  "fac-3": "The area of ​​the facility is 2 hectares",
+  "fac-4": "Construction start: 2019",
+  "fac-5": "Completion of construction: 2020",
+  "fac-6": "Under construction:",
+  "fac-7": "Concrete poured: 850 m³",
+  "fac-8": "Rebar tied: 83 tons",
+  "fac-9": "Metal structures assembled: 275 tons",
+  "fac-10": "Project cost: 3.2 million US dollars",
+  
+
+
+
+   // ---------infra section factory terminal-------------
+ "inf-fac-1": "Infrastructure",
+ "inf-fac-2": "5 500 м² hangar",
+ "inf-fac-3": "65 м³ silo",
+ "inf-fac-4": "Ventilation system",
+ "inf-fac-5": "block painting",
+
+
+
+  // ---------- nature grow section --------------------
+"grow-1": "Nature Grow",
+"grow-2": "Brick manufacturing plant",
+"grow-3": "The area of ​​the facility is 8 hectares",
+"grow-4": "Construction start: 2021",
+"grow-5": "Construction completion: 2023",
+"grow-6": "Under construction:",
+"grow-7": "Concrete poured: 5 400 m³",
+"grow-8": "Rebar tied: 305 tons",
+"grow-9": "Metal structures assembled: 200 tons",
+"grow-10": "Project cost: 8.2 million USD",
+
+
+
+
+
+// ------------infra nature grow section translate
+"infra-grow-1": "Инфраструктура",
+"infra-grow-2": "24 000 м² hangar",
+"infra-grow-3": "2 robots",
+"infra-grow-4": "8 conveyor belts",
+"infra-grow-5": "2 tunnel ovens",
+
+
+// -----------location translate section-----------
+"location": "Tashkent. Sergeli district, St. Sugdiyona, building 4a",
+
+},
+
+
+
+  // =================== ru ============================
   "ru": {
     "selected": "Портфолио",
     "nav-item-1":"Жилой комплекс Нурафшон",
@@ -19,5 +126,114 @@ const translations = {
     "contact":"Контакт",
     "about-h1":"Компания по строительству коммерческих объектов",
     "about-p":"JMAR GREAT PARTNERS  – молодая, динамично развивающаяся строительная компания, специализирующаяся особенно на возведении промышленных объектов. Мы создаем современные, надежные и функциональные производственные площадки, отвечающие всем требованиям бизнеса. Кроме того, наша компания успешно занимается строительством жилых многоэтажных домов, привнося качество и инновационные решения в каждый проект. Также мы оказываем услуги субподрядных работ, что позволяет нашим партнерам эффективно реализовывать самые сложные проекты.",
+    
+   // -----------result section translate-----------
+    "sq-1": "Залито бетона",
+    "sq-2": "2 380 тн",
+    "sq-3": "Связвно aрматур",
+    "sq-4": "475 тн",
+    "sq-5": "Собрано металлоконструций",
+    "sq-6": "Построено",
+
+  // ----------nurafshon section translate---------
+    "nur-1": "Жилой комплекс Нурафшон",
+    "nur-2": "Его общая площадь составляет 4 900 м²",
+    "nur-3": "Начало строительства: 2021 г",
+    "nur-4": "Завершение постройки: 2024 г",
+    "nur-5": "В процессе строительства:",
+    "nur-6": "Залито бетона:  1 550 м³",
+    "nur-7": "Связано арматуры:  140 тн",
+    "nur-8": "Стоимость: 1,3 млн долларов США",
+
+
+   // ----------infra section translate---------
+
+    "inf-1": "Инфраструктура",
+    "inf-2": "28 квартир",
+    "inf-3": "1 005 м² нежилого помещения",
+    "inf-4": "Детская площадка",
+    "inf-5": "Парковка",
+
+
+   // ----------terminal section translate---------
+    "ter-1": "Логистический терминал",
+    "ter-2": "Площадь обьекта составляет 22 гектара",
+    "ter-3": "Начало строительства: 2023 год",
+    "ter-4": "Планируемое завершение: 2026 год",
+    "ter-5": "В процессе строительства:",
+    "ter-6": "Залито бетона: 26 190 м³ ",
+    "ter-7": "Связано арматуры: 1 850 тн",
+    "ter-8": "В процессе строительства будет:",
+    "ter-9": "Залито бетона: 38 000 м³ ",
+    "ter-10": "Связано арматуры: 2 148 тн",
+    "ter-11": "Собрано металлоконструкций: 420 тн",
+    "ter-12": "Стоимость проекта: 30,2 млн долларов США",
+
+
+
+  // ---------infra section logistic terminal--------------
+    "infra-1": "Инфраструктура",
+    "infra-2": "6 000 м² офис",
+    "infra-3": "106 000 м² площадка для котейнеров",
+    "infra-4": "2 козловых крана",
+    "infra-5": "4 570 м железнодорожных путей",
+
+
+
+
+  //------------factory section translate------------------
+  "fac-1": "FACTORY AHK",
+  "fac-2": "Фабрика по производству мебели",
+  "fac-3": "Площадь объекта составляет 2 гектара",
+  "fac-4": "Начало строительства: 2019 год",
+  "fac-5": "Завершение постройки: 2020 год",
+  "fac-6": "В процессе строительства:",
+  "fac-7": "Залито бетона: 850 м³",
+  "fac-8": "Связано арматуры: 83 тн",
+  "fac-9": "Собрано металлоконструкций: 275 тн",
+  "fac-10": "Стоимость проекта: 3,2 млн долларов США",
+  
+
+
+ // ---------infra section factory terminal--------------
+  "inf-fac-1": "Инфраструктура",
+  "inf-fac-2": "5 500 м² ангарa",
+  "inf-fac-3": "65 м³ силоса",
+  "inf-fac-4": "Система вентиляции",
+  "inf-fac-5": "Красочный блок",
+
+
+
+// ---------- nature grow section --------------------
+"grow-1": "Nature Grow",
+"grow-2": "Завод по производству кирпичей",
+"grow-3": "Площадь объекта составляет 8 гектара",
+"grow-4": "Начало строительства: 2021 год",
+"grow-5": "Завершение постройки: 2023 год",
+"grow-6": "В процессе строительства:",
+"grow-7": "Залито бетона: 5 400 м3",
+"grow-8": "Связано арматуры: 305 тн",
+"grow-9": "Собрано металлоконструкций: 200 тн",
+"grow-10": "Стоимость проекта: 8,2 млн долларов США",
+
+
+
+
+
+// ------------infra nature grow section translate
+"infra-grow-1": "Инфраструктура",
+"infra-grow-2": "24 000 м² ангара",
+"infra-grow-3": "2 робота",
+"infra-grow-4": "8 конвейерных лент",
+"infra-grow-5": "2 туннельные печи",
+
+
+
+// -----------location translate section-----------
+"location": "Ташкент. Сергелийский район, Ул. Сугдиёна, дом 4а",
+
+
+
   }
 }
+
